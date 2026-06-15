58116b3f15917b8263a66567eec9309a25ae8ba2
Alexander Kashpir <alexander@kashpir.com>
2026-06-15 19:52:18 +1200

    Restore socket functionality in chatroom

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Code for Free Mart
-A website to sell ownership of pictures for FreeMart Coins (FMC).
+A website to sell ownership of pictures for FreeMart Coins (FMC).  
 I later devloped [a Discord bot](https://github.com/uio23/StonkApplication) to trade these FMC for FreeMart Tokens (FMT).
--- freemart/static/stylesScripts/chatroom.js
+++ freemart/static/stylesScripts/chatroom.js
@@ -1,24 +1,20 @@
 $(document).ready(function(){
+	$('.messages').scrollTop($('.messages')[0].scrollHeight);
+
+	const socket = io.connect();
+
+	socket.on('connect', function(){
+		socket.send({'msg':"I am connected!", 'username': userUsername, 'auth': true});
+	});
+
+	socket.on('message', function(data){
+		$(".messages").append($('<p class="message">').text(data.msg).prepend($('<span class="username"/>').text(`@${data.username} `)));
+		$('.messages').scrollTop($('.messages')[0].scrollHeight);
+	});
 
 
-    $('.messages').scrollTop($('.messages')[0].scrollHeight);
-
-
-    var socket = io.connect("https://" + document.domain + ':' + location.port);
-
-    socket.on('connect', function(){
-        socket.send({'msg':"I am connected!", 'username': userUsername, 'auth': true});
-    });
-
-    socket.on('message', function(data){
-        $(".messages").append($('<p class="message">').text(data.msg).prepend($('<span class="username"/>').text(`@${data.username} `)));
-        $('.messages').scrollTop($('.messages')[0].scrollHeight);
-    });
-
-
-    $(".message-input-btn").on("click", function(){
-        socket.send({'msg':$('.message-input-feild').val(), 'username': userUsername, 'auth': false});
-        $('.message-input-feild').val('');
-    });
-
+	$(".message-input-btn").on("click", function(){
+		socket.send({'msg':$('.message-input-feild').val(), 'username': userUsername, 'auth': false});
+		$('.message-input-feild').val('');
+	});
 });
